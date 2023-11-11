668c8369e2d002a1fc4dc4298f4ad13a00ea3d94
LukeChamberlain <cha22044@byui.edu>
2023-11-11 11:28:59 -0700

    changes word to underscores

--- hangman.py
+++ hangman.py
@@ -1,1 +1,16 @@
-print(f"Welcome to Hangman!")
+import random
+
+print(f"\nWelcome to Hangman!\n")
+words =  ["code", "file", "selection", "view", "terminal", "challenge", "chrome", "british", "football", "cougars"]
+word_of_choice = random.choice(words)
+
+def word_to_underscores(word_of_choice):
+    letters = ""
+    letters += " _"*len(word_of_choice)
+    return letters
+
+word_selected = word_to_underscores(word_of_choice)
+
+print(word_selected)
+print(word_of_choice)
+            
